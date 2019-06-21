96762dbb0b47a91b7509ebcac2c9b432e2afb73c
lyswhut <lyswhut@qq.com>
2019-06-21 17:11:25 +0800

    修复 css-loader 的module选择配置问题

--- build-config/css-loader.config.js
+++ build-config/css-loader.config.js
@@ -1,7 +1,10 @@
 const isDev = process.env.NODE_ENV === 'development'
 
+
 module.exports = {
-  modules: true,
-  localIdentName: isDev ? '[folder]-[name]--[local]-[hash:base64:5]' : '[hash:base64:5]',
-  camelCase: true,
+  modules: {
+    localIdentName: isDev ? '[folder]-[name]--[local]-[hash:base64:5]' : '[hash:base64:5]',
+  },
+  localsConvention: 'camelCase',
 }
+
